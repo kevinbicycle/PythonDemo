5b2fe0597fddb5fd373ed221ae37072c024d7bf6
kevin <liangxinxin001@163.com>
2018-11-20 15:57:00 +0800

    no message

---
+++ kevinPython/venv/bin/kevinDemo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print "hello ,Python"
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print "1 - c 的值为" ,c
+list = [1,2,3,4,5];
+if (a in list):
+    print "变量a在列表中"
+else:
+    print "变量a不在列表中"
+
